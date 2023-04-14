35c7b7a137b6deb7652e5b29dec29cee29211603
Ubuntu <yulun_wu@berkeley.edu>
2023-04-14 22:32:25 +0000

    reset default behavior

--- main.sh
+++ main.sh
@@ -8,11 +8,11 @@ DATA=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 PYARGS=""
 PYARGS="$PYARGS --name train_epoch-1000"
 PYARGS="$PYARGS --data_path $DATA/datasets/marson_prepped.h5ad" #sciplex_prepped.h5ad, L008_prepped.h5ad
-PYARGS="$PYARGS --graph_path $DATA/graphs/marson_grn_64.pth" #sciplex_grn_64.pth, L008_grn_64.pth
+PYARGS="$PYARGS --graph_path $DATA/graphs/marson_grn_128.pth" #sciplex_grn_128.pth, L008_grn_128.pth
 PYARGS="$PYARGS --artifact_path $DATA/artifact"
 PYARGS="$PYARGS --gpu 0" #PYARGS="$PYARGS --cpu"
 
-#PYARGS="$PYARGS --omega1 50.0"
+PYARGS="$PYARGS --omega1 50.0"
 PYARGS="$PYARGS --outcome_dist normal"
 PYARGS="$PYARGS --dist_mode match"
 #PYARGS="$PYARGS --decode_aggr dot"
